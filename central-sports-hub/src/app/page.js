fbe311cc10159b621a0bd8115b09ef9ca7ab416b
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-09 23:12:38 -0600

    changed the title and added metadata, should be good to go when we make tickets

--- central-sports-hub/src/app/page.js
+++ central-sports-hub/src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-zinc-900 font-medium">"Hello World"</h1>
+      <h1 className="text-zinc-900 font-medium">Hello World</h1>
       <Link href="/users">Users</Link>
     </main>
   );
