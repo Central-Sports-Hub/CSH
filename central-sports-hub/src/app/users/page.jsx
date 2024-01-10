753855e285c54070d08949c6fe7461e98e08993a
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-10 13:37:42 -0600

    setting up backend folders and file system

--- central-sports-hub/src/app/users/page.jsx
+++ central-sports-hub/src/app/users/page.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import Link from "next/link";
 
+//handle GET request from /api/users
+app.get("api/users", (req, res) => {
+  const users = [{ id: 1, username: "JohnnyBoy" }];
+  res.json(users);
+});
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
 const UsersPage = () => {
   return <div>Userspage</div>;
 };
