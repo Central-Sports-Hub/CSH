3529e83f7fe9b5cd9f6ac53c5a251ec01283e860
SomaanIqbal <isomaan@gmail.com>
2024-01-12 08:18:36 -0800

    updating seed.js values

--- centralsportshub/src/app/db/seed.js
+++ centralsportshub/src/app/db/seed.js
@@ -15,10 +15,63 @@ const createUser = async() => {
     })
 }
 
+const createOrder = async() => {
+    console.log('creating order');
+
+
+}
+
+const createEvent = async() => {
+    console.log('creating event');
+
+    await prisma.event.createMany({
+        data: [
+            {}
+        ],
+    })
+}
+
+const createTicket = async() => {
+    console.log('creating ticket');
+
+    await prisma.ticket.createMany({
+        data: [
+            {event_id: 1, owner_id: 1, section: 1, seat: 1, row: 1, price: 10.00, date: new Date},
+            {event_id: 2, owner_id: 2, section: 2, seat: 2, row: 2, price: 20.00, date: new Date},
+        ],
+    })
+}
+
+
+const createVenue = async() => {
+    console.log('creating venue');
+
+    await prisma.venue.createMany({
+        data: [
+            {}
+        ],
+    })
+}
+
+const createComment = async() => {
+    console.log('creating comment');
+
+    await prisma.comment.createMany({
+        data: [
+            {}
+        ],
+    })
+}
+
 const main = async() => {
     console.log("seeding the database");
 
     await createUser();
+    // await createOrder();
+    await createTicket();
+    // await createEvent();
+    // await createVenue();
+    // await createComment();
 }
 
 main()
