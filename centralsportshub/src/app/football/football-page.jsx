9d240b214696f698715a6daa8793b6b4a1c0e44f
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-14 20:10:34 -0600

    working on football routes

---
+++ centralsportshub/src/app/football/football-page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Football from "../components/Football";
+
+const page = () => {
+  return (
+    <>
+      <Football />
+    </>
+  );
+};
+
+export default page;
