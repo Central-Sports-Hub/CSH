e844eaf9c3ad45f2b83bfb19c24e9d892ac0bb1c
SomaanIqbal <isomaan@gmail.com>
2024-01-16 14:00:45 -0800

    added folders inside of the api folder for the routes

---
+++ centralsportshub/src/app/api/venues/venues.js
@@ -0,0 +1,16 @@
+import { PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+
+
+        if (req.method === 'GET')  {
+            
+            const venues = await prisma.venues.findMany();
+            
+            res.send(venues)
+            
+    }
+
+    }
