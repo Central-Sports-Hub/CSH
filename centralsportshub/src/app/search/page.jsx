6217ceafa90ae1df5da377ce8c6220ff0ac6863b
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-16 14:35:56 -0600

    adding function and components to the search page.jsx

--- centralsportshub/src/app/search/page.jsx
+++ centralsportshub/src/app/search/page.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import SearchInput from "../searchInput";
+import { useSearchParams } from "next/navigation";
 
 const page = () => {
+  const search = useSearchParams();
+  console.log("SEARCH PARAMS", search);
+
   return (
     <div>
+      SearchComponent
       <SearchInput />
     </div>
   );
