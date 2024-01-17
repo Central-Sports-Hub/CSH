9674002a3873c4f02779d49351003e60a7f0170f
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-17 11:00:09 -0600

    updated the CreateProfile.jsx

--- centralsportshub/src/app/components/createProfile.jsx
+++ centralsportshub/src/app/components/createProfile.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 //create form, onsubmit function to POST data to backend and db entry.
 const createProfile = () => {
-  return <div>createProfile</div>;
+  return (
+    <div className="text-zinc-900 font-medium px-2 text-center">
+      createProfile
+    </div>
+  );
 };
 
 export default createProfile;
