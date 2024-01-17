6b47facf166fe9d00d1a2202ff2d962d17fa516d
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-16 20:03:57 -0600

    cleaning up routes and code to work on profile and dashboard to put request behind login

---
+++ centralsportshub/src/app/(auth)/dashboard/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-zinc-900">DASHBOARD FOR LOGGED IN USER</div>;
+};
+
+export default page;
--- centralsportshub/src/app/users/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div className="text-zinc-900 text-center">USER PAGE</div>;
-};
-
-export default page;
