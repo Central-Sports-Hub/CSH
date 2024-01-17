0529b05bd6f79a2a72a9a43bed0d2bc6923828ad
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-17 10:54:19 -0600

    finished and cleaned up the route.js in the createProfile

--- centralsportshub/src/app/api/createProfile/route.js
+++ centralsportshub/src/app/api/createProfile/route.js
@@ -3,14 +3,25 @@ import prismadb from "@/app/lib/prismadb";
 export async function POST(request) {
   const createUser = async () => {
     console.log("creating user");
-  
-    await prisma.user.create({
-      data: [
-        {
-          email: {""},
-          firstName:{""} ,
-          lastName: {""} ,
-          clerkId: {""},
-          isAdmin: {false},
+
+    try {
+      const user = await prisma.user.create({
+        data: {
+          email: "",
+          firstName: "",
+          lastName: "",
+          clerkId: "",
+          isAdmin: false,
         },
+      });
+
+      console.log("User created:", user);
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
+
+  await createUser();
+
+  return new Response("User created", { status: 201 });
 }
