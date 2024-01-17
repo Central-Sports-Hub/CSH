3944d97a15dbf9dd52e6c5cf1fe03ab322865cef
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-16 21:51:45 -0600

    temp commented out

--- centralsportshub/src/app/components/createProfile.jsx
+++ centralsportshub/src/app/components/createProfile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const createProfile = () => {
-  // prisma.user.create({
-  //   clerkId: [clerkId],
-  //   email: [user.email],
-  //   firstName: [user.firstName],
-  //   lastName: [user.lastName],
-  //   isAdmin: [false],
-  });
-  return <div>Create Profile Page</div>;
-};
+// const createProfile = () => {
+// prisma.user.create({
+//   clerkId: [clerkId],
+//   email: [user.email],
+//   firstName: [user.firstName],
+//   lastName: [user.lastName],
+//   isAdmin: [false],
+// });
+// return <div>Create Profile Page</div>;
+// };
 
-export default createProfile;
+// export default createProfile;
