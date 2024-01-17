d8f1e7f6530eeba49d0f6782793bb7ecec924442
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-16 20:48:45 -0600

    working on auth routes and onboarding profile

--- centralsportshub/src/app/(auth)/dashboard/page.jsx
+++ centralsportshub/src/app/(auth)/dashboard/page.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+//check db for user profile, if no profile show create profile comp and have user fill in info to create profile.
+const { userId } = auth();
+const [onboarded, setOnboarded] = useState(false);
+//query db for user
+const user = await prismadb.user.findUnique({
+  where: {
+    clerkId: userId,
+  },
+});
+
+// may need to be in useeffect.
+// if user .....set onBoarded (false)
+if (!user) {
+  //sets to false to show onboard comp.
+  setOnboarded(false);
+} else {
+  setOnboarded(true);
+}
 
 const page = () => {
+  //if no profile send to create profile page
+  if (!onboarded) return <CreateProfile />;
+  //if user profile exist display content.
   return <div className="text-zinc-900">DASHBOARD FOR LOGGED IN USER</div>;
 };
 
