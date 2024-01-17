557b685090ec4d6c76994df704a70114db2d0b93
SomaanIqbal <isomaan@gmail.com>
2024-01-17 13:27:08 -0800

    venues and venue by id working

---
+++ centralsportshub/src/app/api/venues/route.js
@@ -0,0 +1,13 @@
+import { PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+
+    const venues = await prisma.venues.findMany();
+            
+    return new Response(JSON.stringify(venues));
+            
+    }
+
+    
