ecdf09d184a7bb6a0f90145feadac1e53cf5ac5b
SomaanIqbal <isomaan@gmail.com>
2024-01-18 12:44:42 -0800

    newApi complete

---
+++ src/app/api/venues/[venueId]/route.js
@@ -0,0 +1,17 @@
+import { PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request, { params }) {
+
+    const venueId = parseInt(params.venueId)
+
+    const venue = await prisma.venues.findUnique({
+        where: {
+            id: venueId,
+        }
+    });
+            
+    return new Response(JSON.stringify(venue));
+            
+    }
