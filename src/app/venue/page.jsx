a0741c1b5f620b91e1fe455b832506684511c566
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-18 21:01:50 -0600

    working on building getVEnue route to reach the backend and return venue

---
+++ src/app/venue/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+
+import React from "react";
+
+async function getVenue(sport) {
+  if (sport === "football") {
+  } else {
+    sport === "soccer";
+    return ["stadium 1", "stadium 2"];
+  }
+  return [];
+}
+
+const Venue = async () => {
+  const { venue } = useParams();
+
+  const sport = await getVenue(sport);
+
+  return (
+    <>
+      <h1>venue: {sport}</h1>
+      <div>
+        {venue.map((sport) => (
+          <div>
+            <h2>{venue}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Venue;
