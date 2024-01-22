f81c529269f2303316767b5d3124df412ba75672
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-22 11:43:10 -0600

    adding a fetch call for the venue info from /api/venue

--- src/app/venue/page.jsx
+++ src/app/venue/page.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Event from "../../components/Venue";
 
-const venueData = {
-  name: "Example Venue",
-  location: "Venue Location",
-  homeTeam: "Venue Home Team",
-  capacity: 1000,
-  imgUrl: "https://example.com/image.jpg",
-  comments: ["This is a great venue!", "I can't wait to attend!"],
-  event: {}, // Add any additional event data here
-};
-
 const VenuePage = () => {
-  return (
-    <div>
-      <h1>Venue Page</h1>
-      <Venue {...venueData} />
-    </div>
-  );
-};
+  const [venueData, setVenueData] = useState(null);
 
+  useEffect(() => {
+    fetch("/api/venue")
+      .then((res) => res.json())
+      .then((data) => setVenueData(data));
+  }, []);
+
+  const VenuePage = () => {
+    return (
+      <div>
+        <h1>Venue Page</h1>
+        <Venue {...venueData} />
+      </div>
+    );
+  };
+};
 export default VenuePage;
