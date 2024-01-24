244e121996e8299614bd64b83c969ee2ff8d0c6c
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-23 21:21:32 -0600

    working on spacing the events from the venue on 3000/venues/#

--- src/app/venues/[venueId]/page.js
+++ src/app/venues/[venueId]/page.js
@@ -31,14 +31,14 @@ export default function Venues() {
       <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <h1>Venues</h1>
         <div
-          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          class="mb-2 text-2m font-bold tracking-tight px-6 text-gray-900 dark:text-white"
           className="grid grid-rows-3 grid-flow-col"
         >
           <Venue
             {...venue}
             class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center"
           />
-          <div>
+          <div class="mb-2 text-2m tracking-tight px-10 text-gray-900 dark:text-white">
             <h1> EVENTS:</h1>
             <div className="grid grid-cols-2 items-center">
               {events?.map((event) => (
