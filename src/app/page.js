63ae063c89bf39ebf77424d8225d6e9e8d8fe179
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-01-31 09:50:43 -0600

    changed the homepage text to white

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <div className={styles.background}>
-        <h1>Welcome to my homepage!</h1>
+        <h1 className={styles.whiteText}>Welcome to the Central Sports Hub</h1>
       </div>
     </main>
   );
