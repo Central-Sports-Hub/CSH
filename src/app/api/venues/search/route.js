e91a46b71fa5dd1837f3f4c8a3d4777cc99e2989
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-02-02 10:55:25 -0600

    trying to get cards to render to page

--- src/app/api/venues/search/route.js
+++ src/app/api/venues/search/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
 
   const venues = await prisma.venues.findMany({
     where: {
-      AND: [
+      OR: [
         {
           name: {
             contains: search || "",
