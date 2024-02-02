d757720c8886b26f6938c54fcf6086ea5a89be70
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-02-01 22:27:34 -0600

    added a try catch to the route.js and added useState to the rendering page with try catches still working on getting card to render

--- src/app/api/venues/search/route.js
+++ src/app/api/venues/search/route.js
@@ -5,27 +5,31 @@ const prisma = new PrismaClient();
 export async function GET(request) {
   const { search } = request.query;
 
-  const venues = await prisma.venues.findMany({
-    where: {
-      AND: [
-        {
-          name: {
-            contains: search || "",
+  try {
+    const venues = await prisma.venues.findMany({
+      where: {
+        AND: [
+          {
+            name: {
+              contains: search || "",
+            },
           },
-        },
-        {
-          location: {
-            contains: search || "",
+          {
+            location: {
+              contains: search || "",
+            },
           },
-        },
-        {
-          home_team: {
-            contains: search || "",
+          {
+            home_team: {
+              contains: search || "",
+            },
           },
-        },
-      ],
-    },
-  });
-
-  return new Response(JSON.stringify(venues));
-}
+        ],
+      },
+    });
+  
+    return new Response(JSON.stringify(venues));
+  } catch (error) {
+    console.error("Error fetching venues:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,13 +1,40 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function SearchBar() {
   const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const search = searchParams.get("query");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/search?query=${searchQuery}`);
+        const data = await response.json();
+        setSearchResults(data);
+      } catch (error) {
+        console.error("Error fetching search results:", error);
+      }
+    };
 
-  // URL -> `/dashboard?search=my-project`
-  // `search` -> 'my-project'
-  return <>CSH Results: {search}</>;
+    if (searchQuery) {
+      fetchData();
+    }
+  }, [searchQuery]);
+
+  return (
+    <>
+      <p>CSH Results for: {searchQuery}</p>
+      <div>
+        {searchResults.map((result) => (
+          <div key={result.id}>
+            <h3>{result.title}</h3>
+            <p>{result.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
