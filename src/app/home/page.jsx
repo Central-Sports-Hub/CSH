f703d3fed3ac6e0eb3e2eaea33c89c23631b64dc
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2024-02-07 10:02:53 -0600

    defined styles

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,8 +1,9 @@
 import Home from "@/app/components/Home";
 import React from "react";
 import Link from "next/link";
+import styles from "./home.module.css";
 
-<main className={styles.main - container}>
+<main className={styles.mainContainer}>
   <h1 className="text-zinc-900 font-medium px-2 text-center">
     Welcome to the homepage!
   </h1>
