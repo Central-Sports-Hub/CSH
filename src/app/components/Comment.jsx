06590ac21058c31a7e13da22e3ed6c86e3249946
SomaanIqbal <isomaan@gmail.com>
2024-02-08 12:06:51 -0800

    final commit

--- src/app/components/Comment.jsx
+++ src/app/components/Comment.jsx
@@ -1,9 +1,9 @@
 import { Card } from "react-bootstrap"
 
 
-export default function Comment ({comment_text,created_at}){
+export default function Comment ({comment_text, created_at}){
 return (
-    <div className="card bg-base-100 shadow-xl card-body">
+    <div className="card bg-base-100 shadow-xl card-body mb-1 p-0"  >
         <div>
             {comment_text}
         </div>
@@ -11,6 +11,6 @@ return (
             {created_at.slice(0,10)} 
         </div>
     </div>
-
+    
     )    
 }
